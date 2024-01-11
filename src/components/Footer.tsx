0af593345aebb35f30eb6082655bb1fa499c087f
yeoning <sy03549@gmail.com>
2024-01-11 15:52:42 +0900

    feat: footer 레이아웃 작업

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer__copyright">
+        Made by <b>yeoning</b> (c) 2024 yeoning-k all rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
