8718fe69442b8189be2e8d03eb571be149e9cddf
김수연 <sy03549@gmail.com>
2024-01-28 18:10:49 +0900

    Delete src directory

--- src/components/post/PostEmpty.tsx
+++
@@ -1,5 +0,0 @@
-const PostEmpty = () => {
-  return <div className="empty">게시글이 없습니다 🥲</div>;
-};
-
-export default PostEmpty;
--- src/lib/markdownToHtml.ts
+++
@@ -1,6 +0,0 @@
-import { markdownToTxt } from 'markdown-to-txt';
-
-export const markdownToText = (markdown: string) => {
-  const resultText = markdownToTxt(markdown).substring(0, 150) + '...';
-  return resultText?.split('\n').join('');
-};
