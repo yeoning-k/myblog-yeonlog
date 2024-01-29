145bd26241c7a7e3dccdec3ccf5ce566c955d5d7
yeoning <sy03549@gmail.com>
2024-01-29 18:27:36 +0900

    feat: package next version 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 import { Noto_Sans_KR } from 'next/font/google';
 import Header from './Header';
 
-const NotoSans = Noto_Sans_KR({ subsets: ['latin'] });
+// const NotoSans = Noto_Sans_KR({ subsets: ['latin'] });
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className={NotoSans.className}>
+    <div>
       <Header />
       <div id="container">{children}</div>
     </div>
