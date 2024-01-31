e873245a58b73cb894822b6750cb4d8eecb7b064
yeoning <sy03549@gmail.com>
2024-01-31 23:22:12 +0900

    feat: post 기능을 위한 hook

---
+++ src/lib/hooks.ts
@@ -0,0 +1,51 @@
+import { useMemo, useEffect } from 'react';
+import { allPosts, type Post } from '@contentlayer';
+
+const getAllTags = (data: Post[]) => {
+  const tags = data.map(post => post.tags).flat();
+  return tags.filter((tag, idx) => tags.indexOf(tag) === idx);
+};
+
+export const useGetData = (dir: string) => {
+  const data = useMemo<Post[]>(() => {
+    return allPosts
+      .filter(post => post.category === dir)
+      .sort((a, b) => (a.date > b.date ? -1 : 1));
+  }, [dir]);
+
+  const allTag = useMemo(() => {
+    return getAllTags(data);
+  }, [data]);
+
+  return {
+    posts: data,
+    allTag
+  };
+};
+
+export type ObservationType = Record<string, IntersectionObserverEntry>;
+export const useObserver = (
+  tocList: HTMLElement[],
+  setActiveToc: React.Dispatch<React.SetStateAction<string>>
+) => {
+  useEffect(() => {
+    const observe = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (!entry.isIntersecting) return;
+          setActiveToc(entry.target.id);
+        });
+      },
+      {
+        threshold: 0.3,
+        rootMargin: '0px 0px -90% 0px'
+      }
+    );
+
+    tocList.map(toc => {
+      observe.observe(toc);
+    });
+
+    return () => observe.disconnect();
+  }, [setActiveToc, tocList]);
+};
